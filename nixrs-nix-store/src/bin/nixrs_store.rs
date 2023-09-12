759c273b2322466502b31a715e04909413f1e707
Brian Olsen <brian@maven-group.org>
2023-09-12 16:00:14 +0200

    fix: A simple implementation of nix-store verify

--- nixrs-nix-store/src/bin/nixrs_store.rs
+++ nixrs-nix-store/src/bin/nixrs_store.rs
@@ -1,4 +1,18 @@
+use std::path::PathBuf;
+
+use nixrs_nix_store::verify_path::verify_path;
+use nixrs_store::LegacyLocalStore;
+
 pub fn main() {
-    eprint!("First");
-    todo!();
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let res = runtime
+        .block_on(async move {
+            let store = LegacyLocalStore::connect(true).await?;
+            verify_path(store, &[PathBuf::from("/nix/store/050cxaj0ydhlhgn6f783aah9isg95xiv-autoreconf-hook.drv")]).await
+        });
+
+    res.unwrap();
 }
