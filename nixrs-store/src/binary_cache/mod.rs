6a9af30c093351b5422fe9399ad7786370eaf305
Brian Olsen <brian@maven-group.org>
2023-09-30 13:49:15 +0200

    refactor(store): Move public names in binary_cache around

--- nixrs-store/src/binary_cache/mod.rs
+++ nixrs-store/src/binary_cache/mod.rs
@@ -1,7 +1,9 @@
-pub mod file;
-pub mod http;
+mod file;
+mod http;
 mod traits;
 mod wrap;
 
+pub use self::file::FileBinaryCache;
+pub use self::http::HttpBinaryCache;
 pub use self::traits::BinaryCache;
 pub use self::wrap::BinaryStoreWrap;
