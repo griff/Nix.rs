a0e65fab7e37dc8a31a0864e96f88392ee243127
Brian Olsen <brian@maven-group.org>
2023-10-28 20:22:54 +0200

    feat: Implement LegacyStore for &mut and Box

--- nixrs/src/store/legacy_worker/traits.rs
+++ nixrs/src/store/legacy_worker/traits.rs
@@ -35,3 +35,100 @@ pub trait LegacyStore: Store {
         include_outputs: bool,
     ) -> Result<StorePathSet, Error>;
 }
+
+macro_rules! deref_legacy_store {
+    () => {
+        #[must_use]
+        #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+        fn query_valid_paths_locked<'life0, 'life1, 'async_trait>(
+            &'life0 mut self,
+            paths: &'life1 StorePathSet,
+            lock: bool,
+            maybe_substitute: SubstituteFlag,
+        ) -> ::core::pin::Pin<
+            Box<
+                dyn ::core::future::Future<Output = Result<StorePathSet, Error>>
+                    + ::core::marker::Send
+                    + 'async_trait,
+            >,
+        >
+        where
+            'life0: 'async_trait,
+            'life1: 'async_trait,
+            Self: 'async_trait,
+        {
+            (**self).query_valid_paths_locked(paths, lock, maybe_substitute)
+        }
+
+        #[must_use]
+        #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+        fn export_paths<'life0, 'life1, 'async_trait, SW>(
+            &'life0 mut self,
+            paths: &'life1 StorePathSet,
+            sink: SW,
+        ) -> ::core::pin::Pin<
+            Box<
+                dyn ::core::future::Future<Output = Result<(), Error>>
+                    + ::core::marker::Send
+                    + 'async_trait,
+            >,
+        >
+        where
+            SW: 'async_trait + AsyncWrite + fmt::Debug + Send + Unpin,
+            'life0: 'async_trait,
+            'life1: 'async_trait,
+            Self: 'async_trait,
+        {
+            (**self).export_paths(paths, sink)
+        }
+
+        #[must_use]
+        #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+        fn import_paths<'life0, 'async_trait, SR>(
+            &'life0 mut self,
+            source: SR,
+        ) -> ::core::pin::Pin<
+            Box<
+                dyn ::core::future::Future<Output = Result<(), Error>>
+                    + ::core::marker::Send
+                    + 'async_trait,
+            >,
+        >
+        where
+            SR: 'async_trait + AsyncRead + fmt::Debug + Send + Unpin,
+            'life0: 'async_trait,
+            Self: 'async_trait,
+        {
+            (**self).import_paths(source)
+        }
+
+        #[must_use]
+        #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+        fn query_closure<'life0, 'life1, 'async_trait>(
+            &'life0 mut self,
+            paths: &'life1 StorePathSet,
+            include_outputs: bool,
+        ) -> ::core::pin::Pin<
+            Box<
+                dyn ::core::future::Future<Output = Result<StorePathSet, Error>>
+                    + ::core::marker::Send
+                    + 'async_trait,
+            >,
+        >
+        where
+            'life0: 'async_trait,
+            'life1: 'async_trait,
+            Self: 'async_trait,
+        {
+            (**self).query_closure(paths, include_outputs)
+        }
+    };
+}
+
+impl<T: ?Sized + LegacyStore + Unpin + Send> LegacyStore for Box<T> {
+    deref_legacy_store!();
+}
+
+impl<T: ?Sized + LegacyStore + Unpin + Send> LegacyStore for &mut T {
+    deref_legacy_store!();
+}
