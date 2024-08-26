75a3e3127428b651a74a7a9509ef15b473437914
Brian Olsen <brian@maven-group.org>
2024-08-26 15:25:52 +0200

    chore: Remove unused CollectionSize::empty method

--- nixrs/src/io/collection_size.rs
+++ nixrs/src/io/collection_size.rs
@@ -2,17 +2,11 @@ use std::collections::{BTreeSet, HashSet};
 
 pub trait CollectionSize {
     fn len(&self) -> usize;
-    fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
 }
 
 impl<'a, C> CollectionSize for &'a [C] {
     fn len(&self) -> usize {
         (*self).len()
-    }
-    fn is_empty(&self) -> bool {
-        (*self).is_empty()
     }
 }
 
@@ -23,17 +17,11 @@ where
     fn len(&self) -> usize {
         CollectionSize::len(*self)
     }
-    fn is_empty(&self) -> bool {
-        CollectionSize::is_empty(*self)
-    }
 }
 
 impl<I> CollectionSize for HashSet<I> {
     fn len(&self) -> usize {
         HashSet::len(self)
-    }
-    fn is_empty(&self) -> bool {
-        HashSet::is_empty(self)
     }
 }
 
@@ -41,16 +29,10 @@ impl<I> CollectionSize for BTreeSet<I> {
     fn len(&self) -> usize {
         BTreeSet::len(self)
     }
-    fn is_empty(&self) -> bool {
-        BTreeSet::is_empty(self)
-    }
 }
 
 impl<I> CollectionSize for Vec<I> {
     fn len(&self) -> usize {
         Vec::len(self)
-    }
-    fn is_empty(&self) -> bool {
-        Vec::is_empty(self)
     }
 }
