b7f257f03a98eae06cb8d5c1cdc326e49f9126ff
Brian Olsen <brian@maven-group.org>
2025-04-24 11:27:57 +0200

    chore(derivation): Tests for DerivationOutput::path

--- nixrs/src/derivation/derivation_output.rs
+++ nixrs/src/derivation/derivation_output.rs
@@ -217,3 +217,23 @@ mod daemon_serde {
         }
     }
 }
+
+#[cfg(test)]
+mod unittests {
+    use rstest::rstest;
+
+    use crate::store_path::{StorePath, StorePathNameError};
+
+    use super::DerivationOutput;
+
+    #[rstest]
+    #[case::deffered(DerivationOutput::Deferred, "a", "a", Ok(None))]
+    #[case::input(DerivationOutput::InputAddressed("00000000000000000000000000000000-_".parse().unwrap()), "a", "a", Ok(Some("00000000000000000000000000000000-_".parse().unwrap())))]
+    #[case::fixed_flat(DerivationOutput::CAFixed("fixed:sha256:248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1".parse().unwrap()), "konsole-18.12.3", "out", Ok(Some("g9ngnw4w5vr9y3xkb7k2awl3mp95abrb-konsole-18.12.3".parse().unwrap())))]
+    #[case::fixed_sha1(DerivationOutput::CAFixed("fixed:r:sha1:84983e441c3bd26ebaae4aa1f95129e5e54670f1".parse().unwrap()), "konsole-18.12.3", "out", Ok(Some("ag0y7g6rci9zsdz9nxcq5l1qllx3r99x-konsole-18.12.3".parse().unwrap())))]
+    #[case::fixed_source(DerivationOutput::CAFixed("fixed:r:sha256:248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1".parse().unwrap()), "konsole-18.12.3", "out", Ok(Some("1w01xxn8f7s9s4n65ry6rwd7x9awf04s-konsole-18.12.3".parse().unwrap())))]
+    fn test_path(#[case] output: DerivationOutput, #[case] drv_name: &str, #[case] output_name: &str, #[case] path: Result<Option<StorePath>, StorePathNameError>) {
+        let store_dir = Default::default();
+        assert_eq!(path, output.path(&store_dir, drv_name, output_name))
+    }
+}
